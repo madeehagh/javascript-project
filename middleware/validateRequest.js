0374380ee706af2fc83c115e246d949bc1b1578f
Madeeha Khan <madeeha.khan@dazn.com>
2021-01-28 17:41:12 +0100

    added sql injection patterns

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -2,14 +2,22 @@ const apiError = require('../error/ApiError')
 
 const validateRequest = (req, res, next) => {
 
+    const inputAttacks = /[-!$%^&*()_+|~=`{}\[\]:\/;<>?,.@#]/;
+
     const urlParam = new URLSearchParams(req.query);
 
-    if(!urlParam.has('search')) {
+    if (!urlParam.has('search')) {
         next(apiError.badRequest('search param is required.'));
-    }  else if (urlParam.get('search') === undefined || urlParam.get('search') === '') {
+    } else if (urlParam.get('search') === undefined || urlParam.get('search') === '') {
         next(apiError.badRequest('search value can not be empty.'));
     } else {
-        next();
+        const search = urlParam.get('search');
+        if (search.match(inputAttacks)) {
+            console.log(`A possible injection has been with search value ${search})`)
+            next(apiError.serverError('Request Denied.'));
+        } else {
+            next();
+        }
     }
 }
 

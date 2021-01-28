c74fd059c452ed1a1f8e7b939b4b89d39a51d8a0
Madeeha Khan <madeeha.khan@dazn.com>
2021-01-28 09:10:17 +0100

    added response var

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,6 +1,6 @@
 const apiError = require('../error/ApiError')
 
-const validateRequest = (req, next) => {
+const validateRequest = (req, res, next) => {
 
     const urlParam = new URLSearchParams(req.query);
 
@@ -9,6 +9,7 @@ const validateRequest = (req, next) => {
     }  else if (urlParam.get('search') === undefined || urlParam.get('search') === '') {
         next(apiError.badRequest('search value can not be empty.'));
     } else {
+        console.log("req " + req);
         next();
     }
 }
